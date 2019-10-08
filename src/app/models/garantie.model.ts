b921961e8c40ce5abd3e7793ee4bf66e1c6bf970
RENOULEAU Cedric <cedric_renouleau@ad>
2019-10-08 09:32:40 +0200

    add garantie model

---
+++ src/app/models/garantie.model.ts
@@ -0,0 +1,9 @@
+export class Garantie {
+  position: number;
+  name: string;
+  additionalInfo?: string;
+  formuleOne?: boolean;
+  formuleTwo?: boolean;
+  formuleThree?: boolean;
+  isDisabled?: boolean;
+}
